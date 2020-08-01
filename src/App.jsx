436ce891a2e05c3524fc96dd72d97e7e419ee204
Jeff <zjyeung579@gmail.com>
2020-08-01 16:01:45 -0700

    Implemented scatter plot from react-chartjs-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,126 @@
 import React from 'react';
-import { Bubble } from 'react-chartjs-2';
-import './App.css';
+import { Scatter } from 'react-chartjs-2';
+import styled from 'styled-components';
+import dimensions from './styles/dimensions';
 
 const data = {
-  labels: ['Andy', 'Jeff', 'Chris', 'Henry', 'Samuel'],
+  labels: ['Player1', 'Player2', 'Player3', 'Player4', 'Player5', 'Player6', 'Player7', 'Player11', 'Player12', 'Player13', 'Player14', 'Player15', 'Player16', 'Player17'],
   datasets: [
     {
-      label: 'My First dataset',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
+      label: 'Houston Rockets',
+      labels: ['1', '2', '3', '4', '5', '6', '7'],
+      fill: true,
+      backgroundColor: 'rgba(234, 48, 73, 1)',
+      pointBorderColor: 'rgba(234, 48, 73, 1)',
       pointBackgroundColor: '#fff',
       pointBorderWidth: 1,
       pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBackgroundColor: 'rgba(234, 48, 73, 1)',
       pointHoverBorderColor: 'rgba(220,220,220,1)',
       pointHoverBorderWidth: 2,
-      pointRadius: 1,
+      pointRadius: 5,
       pointHitRadius: 10,
-      data: [{x:10,y:20,r:5}]
-    }
+      data: [
+        { x: 65, y: 75 },
+        { x: 59, y: 49 },
+        { x: 80, y: 90 },
+        { x: 81, y: 29 },
+        { x: 56, y: 36 },
+        { x: 55, y: 25 },
+        { x: 40, y: 18 },
+      ]
+    },
+    {
+      label: 'New Orleans Pelicans',
+      labels: ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+      fill: true,
+      backgroundColor: 'rgba(11, 35, 63, 1)',
+      pointBorderColor: 'rgba(11, 35, 63, 1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(11, 35, 63, 1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 5,
+      pointHitRadius: 10,
+      data: [
+        { x: 35, y: 35 },
+        { x: 59, y: 89 },
+        { x: 80, y: 50 },
+        { x: 31, y: 59 },
+        { x: 56, y: 36 },
+        { x: 33, y: 25 },
+        { x: 40, y: 28 },
+      ]
+    },
   ]
 };
+
+const options = {
+  responsive: true,
+  tooltips: {
+    mode: 'nearest',
+    callbacks: {
+      label: function(toolTipItem, data) {
+        let label = data.datasets[toolTipItem.datasetIndex].labels[toolTipItem.index] || '';
+
+        if (label) label += ': ( ' + toolTipItem.xLabel + ', ' + toolTipItem.yLabel + ')';
+
+        return label;
+      }
+    }
+  },
+  scales: {
+    xAxes: [
+      {
+        display: true,
+        labels: {
+          show: true
+        }
+      }
+    ],
+    yAxes: [
+      {
+        display: true,
+        labels: {
+          show: true
+        }
+      }
+    ]
+  }
+};
+
+const LayoutContainer = styled.div`
+    max-width: ${dimensions.maxwidthDesktop}px;
+    padding-left: ${dimensions.paddingHorizontalDesktop}em;
+    padding-right: ${dimensions.paddingHorizontalDesktop}em;
+    margin: 0 auto;
+
+    @media(max-width: ${dimensions.maxwidthTablet}px) {
+        padding-left: ${dimensions.paddingHorizontalTablet}em;
+        padding-right: ${dimensions.paddingHorizontalTablet}em;
+    }
+
+    @media(max-width: ${dimensions.maxwidthMobile}px) {
+        padding-left: ${dimensions.paddingHorizontalMobile}em;
+        padding-right: ${dimensions.paddingHorizontalMobile}em;
+    }
+
+    .Layout__content {
+        padding-bottom: 5em;
+    }
+`;
 
 
 function App() {
   return (
-    <div className="App">
+    <LayoutContainer className="App">
       <h1>Basketball Stats</h1>
       <div>
-        <Bubble data={data} />
+        <Scatter data={data} options={options}/>
       </div>
-    </div>
+    </LayoutContainer>
   );
 }
 
